bf096d3f6a022144efa7074d5545a009072ade59
PenRockCard <PRC@Desktop>
2023-01-06 20:52:49 -0500

    Updated references so it doesn't reference a previous test project.

--- FirstGame/Gui_Test.h
+++ FirstGame/Gui_Test.h
@@ -3,9 +3,9 @@
 #include <compare>
 #include <vector>
 
-#include "imgui.h"
-#include "imgui_impl_win32.h"
-#include "imgui_impl_dx12.h"
+#include "imgui\imgui.h"
+#include "imgui\imgui_impl_win32.h"
+#include "imgui\imgui_impl_dx12.h"
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <tchar.h>
