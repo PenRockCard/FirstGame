b557b3946dba42c60cf7081f6204dedbbc388580
PenRockCard <PRC@Desktop>
2023-01-08 16:49:01 -0500

    Added comments to the Update_Time class

--- FirstGame/Update_Time.cpp
+++ FirstGame/Update_Time.cpp
@@ -1,13 +1,29 @@
+/*
+A class designed to measure, set and get the time it takes to update the game.
+*/
+
 #include "Update_Time.h"
 
+/*
+The default constructor. Sets the minimum update time to 0.5 seconds.
+TODO: Add another constructor to set it based on the time input?
+*/
 Update_Time::Update_Time() {
 	timePeriod = nanoseconds(500000000);
 };
 
+/*
+Sets the start time for the beginning of the update.
+*/
 void Update_Time::UpdateStart() {
 	updateStartTime = high_resolution_clock::now();
 }
 
+/*
+Finishes the update. Gets the current time, and compares it to the beginning time. This variable can be gotten via a getter below.
+Then it'll sleep the thread long enough to meet the minimum update time.
+TODO: Figure out what to do when the update time is shorter than the time it took to update the game?
+*/
 void Update_Time::UpdateFinish() {
 	updateFinishTime = high_resolution_clock::now();
 
@@ -18,11 +34,17 @@ void Update_Time::UpdateFinish() {
 	this_thread::sleep_for(timePeriod - updateTotalTime);
 }
 
+/*
+Sets the minimum update time.
+*/
 bool Update_Time::SetUpdateTime(long long newUpdateTime) {
 	timePeriod = nanoseconds(newUpdateTime);
 	return true;
 }
 
+/*
+Returns the time it actually took to update the game.
+*/
 long long Update_Time::GetUpdateTime() {
 	return updateTotalTime.count();
 }
--- FirstGame/Update_Time.h
+++ FirstGame/Update_Time.h
@@ -7,9 +7,16 @@ using namespace chrono;
 
 class Update_Time
 {
-	chrono::nanoseconds timePeriod; // 0.5 sec
+	//The current minimum update time.
+	chrono::nanoseconds timePeriod; 
+
+	//The start time of the current update.
 	steady_clock::time_point updateStartTime;
+
+	//The finish time of the update.
 	steady_clock::time_point updateFinishTime;
+
+	//How long it took to update the game
 	nanoseconds updateTotalTime;
 
 public:
